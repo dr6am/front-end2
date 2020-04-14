6af789e1451c194058e04bca50b8ec868a872660
Alex <46999226+dr6am@users.noreply.github.com>
2020-04-14 21:09:23 +0300

    implemented mirror statistics and fix mobile version

---
+++ precache-manifest.fda7b526f4c4dbf4023ec32475ccfea7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d60f213bfbd9dccd5c3e652a18d412ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af6a87c9c3ddca681390",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "10e8dde00f865c03f502",
+    "url": "/static/css/main.57a2060e.chunk.css"
+  },
+  {
+    "revision": "af6a87c9c3ddca681390",
+    "url": "/static/js/2.81e052b2.chunk.js"
+  },
+  {
+    "revision": "ca3cfcda77dc9f4f71fda9fdfcf14fb0",
+    "url": "/static/js/2.81e052b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05c8468eea0203a009ca",
+    "url": "/static/js/3.2671134d.chunk.js"
+  },
+  {
+    "revision": "e5e9215a296494a67905",
+    "url": "/static/js/4.70f34c6d.chunk.js"
+  },
+  {
+    "revision": "10e8dde00f865c03f502",
+    "url": "/static/js/main.afdcc756.chunk.js"
+  },
+  {
+    "revision": "8638e7a00b9be15fc835",
+    "url": "/static/js/runtime-main.c2028b81.js"
+  }
+]);
